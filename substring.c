46e382efbd0f0f50f34a770837e8006d4b87d4c4
Italo Bruno <italo23042001@gmail.com>
2024-12-01 06:28:13 -0300

    tentativa 2

--- substring.c
+++ substring.c
@@ -3,38 +3,33 @@
 
 #define ALPHABET_SIZE 26
 
+void calcularFrequencias(char* palavra, int tamanho, int frequencias[]) {
+    for (int i = 0; i < tamanho; i++) {
+        frequencias[palavra[i] - 'a']++;
+    }
+}
+
 long long validSubstringCount(char* word1, char* word2) {
     int len1 = strlen(word1), len2 = strlen(word2);
     if (len2 > len1) return 0;
 
     int freq2[ALPHABET_SIZE] = {0};
-    for (int i = 0; i < len2; i++) {
-        freq2[word2[i] - 'a']++;
-    }
+    calcularFrequencias(word2, len2, freq2);
 
     int freqWindow[ALPHABET_SIZE] = {0};
     long long count = 0;
 
-    for (int i = 0; i < len2; i++) {
-        freqWindow[word1[i] - 'a']++;
-    }
-
-    int valid = 1;
-    for (int i = 0; i < ALPHABET_SIZE; i++) {
-        if (freqWindow[i] < freq2[i]) {
-            valid = 0;
-            break;
+    for (int i = 0; i <= len1 - len2; i++) {
+        if (i == 0) {
+            calcularFrequencias(word1, len2, freqWindow);
+        } else {
+            freqWindow[word1[i - 1] - 'a']--;
+            freqWindow[word1[i + len2 - 1] - 'a']++;
         }
-    }
-    if (valid) count++;
 
-    for (int i = len2; i < len1; i++) {
-        freqWindow[word1[i - len2] - 'a']--;
-        freqWindow[word1[i] - 'a']++;
-
-        valid = 1;
+        int valid = 1;
         for (int j = 0; j < ALPHABET_SIZE; j++) {
-            if (freqWindow[j] < freq2[j]) {
+            if (freqWindow[j] != freq2[j]) {
                 valid = 0;
                 break;
             }
