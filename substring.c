50b0aceb8e27c61edf90fe737e6f70078aee4c13
Italo Bruno <italo23042001@gmail.com>
2024-12-01 06:51:38 -0300

    Aqui foi

--- substring.c
+++ substring.c
@@ -3,39 +3,39 @@
 
 #define ALPHABET_SIZE 26
 
-void calcularFrequencias(char* palavra, int tamanho, int frequencias[]) {
-    for (int i = 0; i < tamanho; i++) {
-        frequencias[palavra[i] - 'a']++;
-    }
-}
-
 long long validSubstringCount(char* word1, char* word2) {
-    int len1 = strlen(word1), len2 = strlen(word2);
-    if (len2 > len1) return 0;
+    unsigned int need = 0, have = 0;
+    int count[26] = {0}, missing = 0, left = 0;
+    long long total = 0;
 
-    int freq2[ALPHABET_SIZE] = {0};
-    calcularFrequencias(word2, len2, freq2);
+    for (int i = 0; word2[i]; i++) {
+        int idx = word2[i] - 'a';
+        if (!(need & (1 << idx))) missing++;
+        need |= 1 << idx;
+        count[idx]++;
+    }
 
-    int freqWindow[ALPHABET_SIZE] = {0};
-    long long count = 0;
-
-    for (int i = 0; i <= len1 - len2; i++) {
-        if (i == 0) {
-            calcularFrequencias(word1, len2, freqWindow);
-        } else {
-            freqWindow[word1[i - 1] - 'a']--;
-            freqWindow[word1[i + len2 - 1] - 'a']++;
-        }
-
-        int valid = 1;
-        for (int j = 0; j < ALPHABET_SIZE; j++) {
-            if (freqWindow[j] != freq2[j]) {
-                valid = 0;
-                break;
+    for (int i = 0; word1[i]; i++) {
+        int idx = word1[i] - 'a';
+        if (need & (1 << idx)) {
+            count[idx]--;
+            if (count[idx] == 0) {
+                have |= 1 << idx;
+                missing--;
             }
         }
-        if (valid) count++;
+
+        if (missing == 0) {
+            while (left <= i) {
+                int left_idx = word1[left] - 'a';
+                if (!(need & (1 << left_idx)) || count[left_idx] < 0) {
+                    if (need & (1 << left_idx)) count[left_idx]++;
+                    left++;
+                } else break;
+            }
+            total += left + 1;
+        }
     }
 
-    return count;
+    return total;
 }
